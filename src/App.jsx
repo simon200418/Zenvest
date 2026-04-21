7433e962e2b21ec8262ac6d3d785d98c7106f673
simon200418 <simonpathre20@gmail.com>
2026-04-21 20:25:34 +0530

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+// import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Stock Application</h1>
+        
+    </>
+  )
+}
+
+export default App
