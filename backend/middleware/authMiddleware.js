7eb16dce3c3dc11e2de7497a8b1622b11b0bcde2
simon200418 <simonpathre20@gmail.com>
2026-06-02 23:49:41 +0530

    Fix Vercel Routing

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -22,14 +22,12 @@ const authMiddleware = (req, res, next) => {
   }
 
   try {
-
     const decoded = jwt.verify(
       token,
       process.env.JWT_SECRET
     );
 
     req.user = decoded;
-
     next();
 
   } catch (err) {
