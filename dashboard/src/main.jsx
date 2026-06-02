b3e87fbcf95325e2c84bebf7e00f33da213ada3b
simon200418 <simonpathre20@gmail.com>
2026-06-02 23:55:56 +0530

    Fix Vercel Routing

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
-import Home from "./components/Home";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
