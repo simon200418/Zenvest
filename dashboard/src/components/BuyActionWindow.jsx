c2fc356ba1f5f851bee265fe91337e82821f5a4c
simon200418 <simonpathre20@gmail.com>
2026-06-03 00:24:13 +0530

    new Order changes

--- dashboard/src/components/BuyActionWindow.jsx
+++ dashboard/src/components/BuyActionWindow.jsx
@@ -64,12 +64,12 @@ const BuyActionWindow = ({ uid }) => {
       <div className="buttons">
         <span>Margin required ₹140.65</span>
         <div>
-          <Link className="btn btn-blue" onClick={handleBuyClick}>
+          <button className="btn btn-blue" onClick={handleBuyClick}>
             Buy
-          </Link>
-          <Link to="" className="btn btn-grey" onClick={handleCancelClick}>
+          </button>
+          <button to="" className="btn btn-grey" onClick={handleCancelClick}>
             Cancel
-          </Link>
+          </button>
         </div>
       </div>
     </div>
